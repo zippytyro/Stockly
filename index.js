642eec5c6bb499a2ffc7448184f8e379fcea97e7
Shashwat verma <42150187+zippytyro@users.noreply.github.com>
2020-04-27 01:16:56 +0530

    Adding initial Project files.

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav-bar").style.width = "250px";
+    document.getElementById("nav-bar").style.backgroundColor = "#512b58E6";
+    document.body.style.backgroundColor = "#512b5880";
+}
+
+function closeNav() {
+    document.getElementById("nav-bar").style.width = "0";
+    document.getElementById("nav-bar").style.backgroundColor = "transparent";
+    document.body.style.backgroundColor = "transparent";
+}
