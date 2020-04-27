b1c870587aa22195ba912c8dff6a351159799b83
Shashwat verma <42150187+zippytyro@users.noreply.github.com>
2020-04-27 15:38:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("nav-bar").style.width = "250px";
-    document.getElementById("nav-bar").style.backgroundColor = "#512b58E6";
+    document.getElementById("nav-bar").style.backgroundColor = "#512b58";
     document.body.style.backgroundColor = "#512b5880";
 }
 
